2de4030ce116a5812018e3b6ac65108b11aa8f23
Dongwoo-Kim <dongeu47@gmail.com>
2023-01-01 16:54:04 +0900

    temp
    temp

--- server/src/controllers/product.controller.ts
+++ server/src/controllers/product.controller.ts
@@ -4,5 +4,6 @@ export default class ProductController {
   static addProduct = async (req: Request, res: Response, next: NextFunction) => {
     const userDTO = JSON.parse(req.body.addProductData);
     const images = req.files;
+    console.log(userDTO, images);
   };
 }
